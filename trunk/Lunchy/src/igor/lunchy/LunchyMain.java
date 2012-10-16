f3d44ec291b4d7ab8a1aae47168126ecd55995c3
igor9mail@gmail.com <igor9mail@gmail.com@e0040940-3af0-a6ae-6b64-2955329664a2>
2012-10-16 11:42:00 +0000

    First version

---
+++ trunk/Lunchy/src/igor/lunchy/LunchyMain.java
@@ -0,0 +1,132 @@
+/*
+ *   Main Form (Menu) for current project 
+ * 
+ */
+
+package igor.lunchy;
+
+import org.eclipse.swt.*;
+import org.eclipse.swt.events.*;
+import org.eclipse.swt.graphics.*;
+import org.eclipse.swt.layout.*;
+import org.eclipse.swt.widgets.*;
+
+
+public class LunchyMain {
+	
+	private Shell shell;
+	public String status = "";
+	
+	
+	public static void main(String[] args) {
+		Display display = new Display();
+		LunchyMain application = new LunchyMain();
+		Shell shell = application.open(display);
+		System.out.println("OK");
+		/*
+		String[][] Data = ParseTextFile.getStringTable("menu.txt", "_", 6);
+		int i = 0;
+		System.out.println(Data.length);
+		for (String[] str: Data) {
+			for (String str2: str) {
+				System.out.print(str2 + " ");
+			}
+			System.out.println();
+		}
+		
+		MenuEnrty[] menu = MenuEnrty.getMenuEntries("menu.txt");
+		System.out.println(menu[4].name);
+		*/
+		
+		while(!shell.isDisposed()){
+			if(!display.readAndDispatch())
+				display.sleep();
+		}
+		display.dispose();
+			
+	}
+	
+	public Shell open(Display display) {
+		shell = new Shell(display, SWT.DIALOG_TRIM | SWT.MIN);
+		RowLayout layout = new RowLayout(SWT.VERTICAL);
+		layout.spacing = 10;
+		layout.marginLeft = 10;
+		layout.marginRight = 10;
+		layout.marginTop = 10;
+		layout.marginBottom = 10;
+		layout.pack = false;
+		shell.setLayout(layout);
+		
+		Label status =  createControlButtons();
+		status.setText("OK");
+		
+		Rectangle clientArea = display.getClientArea();
+		shell.setText("  Lunchy");
+		shell.setLocation(clientArea.width/2-80, clientArea.height/2-150);
+		shell.setImage(new Image(display, "icon2.jpg"));
+		shell.pack();
+		shell.open();
+		
+		return shell;
+	}
+	
+	private Label createControlButtons() {
+		Button btMenuEditor = new Button(shell, SWT.PUSH);
+		btMenuEditor.setText("Edit menu");
+		btMenuEditor.setLayoutData(new RowData(170, 40));
+		btMenuEditor.addSelectionListener(new SelectionAdapter() {
+			public void widgetSelected(SelectionEvent e) {
+				openEditMenu();
+			}
+		});
+		
+		Button btOrder = new Button(shell, SWT.PUSH);
+		btOrder.setText("Make order");
+		btOrder.addSelectionListener(new SelectionAdapter() {
+			public void widgetSelected(SelectionEvent e) {
+				openEditMenu();
+			}
+		});
+		
+		Button btSendOrder = new Button(shell, SWT.PUSH);
+		btSendOrder.setText("Send Order");
+		btSendOrder.addSelectionListener(new SelectionAdapter() {
+			public void widgetSelected(SelectionEvent e) {
+				openEditMenu();
+			}
+		});
+		
+		Button btPrint = new Button(shell, SWT.PUSH);
+		btPrint.setText("Print Order");
+		btPrint.addSelectionListener(new SelectionAdapter() {
+			public void widgetSelected(SelectionEvent e) {
+				openEditMenu();
+			}
+		});
+		
+		Button btOptions = new Button(shell, SWT.PUSH);
+		btOptions.setText("Options");
+		btOptions.addSelectionListener(new SelectionAdapter() {
+			public void widgetSelected(SelectionEvent e) {
+				openEditMenu();
+			}
+		});
+		
+		Label statusLabel = new Label(shell, SWT.BORDER);
+		statusLabel.setText(status);
+		
+		
+				
+		//DateTime dt = new DateTime(shell, SWT.CALENDAR);
+		//statusLabel.setLayoutData(new RowData(150, 60));
+		
+		return statusLabel;
+	}
+	
+	private void openEditMenu() {
+		//System.out.println("All OK");
+		MenuEditForm menuForm = new MenuEditForm(shell);
+		menuForm.open();
+	}
+
+}
---
+++ trunk/Lunchy/src/igor/lunchy/ParseTextFile.java
@@ -0,0 +1,142 @@
+package igor.lunchy;
+
+import java.io.*;
+//import java.util.*;
+
+public class ParseTextFile {
+	
+	public ParseTextFile() {
+		super();
+	}
+	
+	private static String encodeLine(String[] tableItems, String delimiter) {
+		String line = "";
+		for (int i = 0; i < tableItems.length - 1; i++) {
+			line += tableItems[i] + delimiter;
+		}
+		line += tableItems[tableItems.length - 1] + "\r\n";
+		
+		return line;
+	}
+	
+	private static String[] decodeLine(String line, String delimiter, int rowCount) {
+		if(line == null)
+			return null;
+		
+		String[] parsedLine = new String[rowCount];
+		for(int i = 0; i < parsedLine.length - 1; i++) {
+			int index = line.indexOf(delimiter);
+			if (index > -1) {
+				parsedLine[i] = line.substring(0, index);
+				line = line.substring(index + delimiter.length(), line.length());
+			} else {
+				return null;
+			}
+		}
+		
+		if (line.indexOf(delimiter) != -1)
+			return null;
+		parsedLine[parsedLine.length - 1] = line;
+		return parsedLine;
+	}
+	
+	static public String[][] getStringTable(String filename, String delimiter, int rowCount) {
+
+		if(filename == null)
+			return null;
+		
+		
+		File file = new File(filename);
+		if (!file.exists()) {
+			System.out.println("File not exist"); 
+			return null;
+		}
+		
+		FileReader fileReader = null;
+		BufferedReader bufferedReader = null;
+		String[] data = new String[0];
+		try {
+			fileReader = new FileReader(file.getAbsolutePath());
+			bufferedReader = new BufferedReader(fileReader);
+			String nextLine = bufferedReader.readLine();
+			while (nextLine != null){
+				String[] newData = new String[data.length + 1];
+				System.arraycopy(data, 0, newData, 0, data.length);
+				newData[data.length] = nextLine;
+				data = newData;
+				nextLine = bufferedReader.readLine();
+			}
+		} catch(FileNotFoundException e) {
+			System.out.println("File not found");
+			return null;
+		} catch (IOException e ) {
+			System.out.println("IO read error");
+			return null;
+		} finally {	
+			if(fileReader != null) {
+				try {
+					fileReader.close();
+				} catch(IOException e) {
+					System.out.println("IO close error");
+					return null;
+				}
+			}
+		}
+		
+		String[][] tableInfo = new String[data.length][rowCount];
+		int writeIndex = 0;
+		for (int i = 0; i < data.length; i++) {
+			String[] line = decodeLine(data[i], delimiter, rowCount);
+			if (line != null) tableInfo[writeIndex++] = line;
+		}
+		if (writeIndex != data.length) {
+			String[][] result = new String[writeIndex][rowCount];
+			System.arraycopy(tableInfo, 0, result, 0, writeIndex);
+			tableInfo = result;
+		}
+		return tableInfo;
+	}
+	
+	static public boolean putStringTableToFile(String filename, String[][] data) {
+		
+		if(filename == null)
+			return false;
+				
+		File file = new File(filename);
+		if (!file.exists()) {
+			System.out.println("File not exist"); 
+			return false;
+		}
+		
+		String[] lines = new String[data.length];
+		for(int i = 0; i < data.length; i++) {
+			lines[i] = encodeLine(data[i],"_");
+		}
+			
+		FileWriter fileWriter = null;
+		try { 
+			fileWriter = new FileWriter(file.getAbsolutePath(), false);
+			for (int i = 0; i < lines.length; i++) {
+				fileWriter.write(lines[i]);
+			}
+		} catch(FileNotFoundException e) {
+			System.out.println("File not found");
+			return false;
+		} catch(IOException e ) {
+			System.out.println("IO write error");
+			return false;
+		} finally {
+			
+			if(fileWriter != null) {
+				try {
+					fileWriter.close();
+				} catch(IOException e) {
+					System.out.println("IO close error");
+					return false;
+				}
+			}
+		}
+		
+		return true;
+	}
+}
