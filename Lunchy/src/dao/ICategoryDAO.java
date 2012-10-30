39c0f897da1cbe5c9e25f74d4808a48d7afeb31e
aist11@gmail.com <aist11@gmail.com@e0040940-3af0-a6ae-6b64-2955329664a2>
2012-10-30 14:27:09 +0000

    mentor's fixes

--- Lunchy/src/dao/ICategoryDAO.java
+++ Lunchy/src/dao/ICategoryDAO.java
@@ -5,6 +5,10 @@ import java.util.ArrayList;
 import entities.Category;;
 
 public interface ICategoryDAO {
-	public ArrayList<Category> getCategoryCol();
-	public boolean putCategoryCol(ArrayList<Category> categoryCol);
+	ArrayList<Category> getCategoryCol();
+	boolean putCategoryCol(ArrayList<Category> categoryCol);
+	
+	Category createCategory();
+	void removeCategory(int categoryId);
+	void updateCategory(Category category);
 }
